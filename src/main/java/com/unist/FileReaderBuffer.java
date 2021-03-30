3fdc958fe0b16e3912aa884595c7113ced182989
int2k <dungduychinguyen@gmail.com>
2021-03-31 03:03:36 +0900

    buffered reading implemented

--- src/main/java/com/unist/FileReaderBuffer.java
+++ src/main/java/com/unist/FileReaderBuffer.java
@@ -3,16 +3,21 @@ package com.unist;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileReaderBuffer {
     public String[] readFile(String path) throws IOException {
-        BufferedReader reader;
-        reader = new BufferedReader(new FileReader(path));
+        BufferedReader reader = new BufferedReader(new FileReader(path));;
+        List<String> tmp = new ArrayList<String>();
         String line = reader.readLine();
         while(line != null) {
-            System.out.println(line);
+            tmp.add(line);
             line = reader.readLine();
         }
         reader.close();
+        String[] lines = new String[tmp.size()];
+        tmp.toArray(lines);
+        return lines;
     }
 }
