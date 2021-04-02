45df13d6b4adcfe75e1a85a99641501397aec55e
Luong Doan <doannguyenthanhluong@gmail.com>
2021-04-02 17:55:47 +0700

    Handle wrong order args

--- src/main/java/com/unist/App.java
+++ src/main/java/com/unist/App.java
@@ -11,17 +11,25 @@ public class App {
 
     public static void main (String[] args) throws IOException {
         // args = new String[]{"Adventure", "educator"};
+        // args = new String[]{"Comedy", "educator"};
+        // args = new String[]{"educator", "Adventure"};
         System.out.println();
         if(args.length <= 0) {
             System.out.println("Project compiled successfully, please input 2 args (genres occupation) to run");
         }
-        else if (args.length >= 2){
+        else {
             if(args.length > 2) {
                 System.out.println("More than 2 arguments inputted, program will only" +
                         "consider the first two");
             }
             Milestone1 solver = new Milestone1();
             solver.initialize();
+
+            if (solver.isWrongOrder(args)) {
+                System.out.println("You should input genre before occupation");
+                return;
+            }
+            
             solver.solve(args);
             double output = solver.output();
             if (output != -1) {
