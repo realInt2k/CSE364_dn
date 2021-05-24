10f90c8e1b243f128a6e6618ee0afbb6f3b2f2c1
truongnhatnguyenbao <62552346+truongnhatnguyenbao@users.noreply.github.com>
2021-05-25 02:32:39 +0700

    Update run.sh for Milestone3

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 git clone https://github.com/realInt2k/CSE364_dn.git
 cd CSE364_dn
-mvn install
-java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar com.unist.App "M" "30" "Doctor"
-java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar com.unist.App "F" "27" "Marketing" "Romance"
+mvn spring-boot:run
