aa34c3dd4e5d8f9cec068712748d53dc493499d7
dungn <dungduychinguyen@gmail.com>
2021-06-12 03:26:04 +0900

    display less = more responsive

--- src/main/resources/public/movieBased.js
+++ src/main/resources/public/movieBased.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-
+    let cnt = 0;
     $("#query").on("click", function() {
         let title = $("#title").val();
         let limit = $("#limit").val();
         let query = "http://localhost:8080/movies/recommendations?";
         query += "title="+title;
         query += "&limit="+limit;
-        // console.log(query);
+        cnt = 0;
         $.ajax({
             url: query,
             dataType: "json"
@@ -25,32 +25,54 @@ $(document).ready(function() {
                     );
                 }
                 let content = "";
-                let cnt = 0;
                 let w = 100;
                 let h = 150;
-                for(let i in data.movieList) {
-                    cnt += 1;
-                    $(".MovieContainer").append(function(){return "<div " +
-                        "id="+("Image_"+cnt)+
-                        " href=" + data.movieList[i].imdb + "></div>"}).after(function(){
-                        if(data.movieList[i].imageLink) {
-                            loadImage(data.movieList[i].imageLink, w, h, $("#Image_"+cnt), data.movieList[i].title);
-                        } else {
-                            loadImage("where.png", w, h, $("#Image_"+cnt), data.movieList[i].title);
+                display();
+                function display()
+                {
+                    let myobj = document.getElementById("SeeMore");
+                    if(myobj)
+                        myobj.remove();
+                    for(let i = cnt; i< data.movieList.length; ++i) {
+                        cnt += 1;
+                        if(cnt % 50 === 0) {
+                            $(".MovieContainer").append(
+                                "<button id=\"SeeMore\"" +
+                                "style=\"background-color: green; " +
+                                    "font-size: 16px; border-radius: 8px; " +
+                                    "color:white; " +
+                                    "padding: 15px 24px; text-align: center; margin: auto; width: 100%;\">" +
+                                "Click to view more..." +
+                                "</button> <br>"
+                            )
+                            $("#SeeMore").on("click", function(){
+                                console.log("clicked");
+                                display();
+                            })
+                            break;
                         }
-                    });
-                    $("#Image_"+cnt).on('click','img',function(){
-                        window.open(data.movieList[i].imdb);
-                    });
-                }
-                function loadImage(path, width, height, target, title = "No title") {
-                    $("<p>        </p><br>").appendTo(target);
-                    $("<p>"+title+"</p>").appendTo(target);
-                    $('<img src="'+ path +'">').load(function() {
-                        $(this).width(width).height(height).appendTo(target);
-                    }).error(function(e) {
-                        $(this).attr("src", "where.png").width(width).height(height);
-                    });
+                        $(".MovieContainer").append(function(){return "<div " +
+                            "id="+("Image_"+cnt)+
+                            " href=" + data.movieList[i].imdb + "></div>"}).after(function(){
+                            if(data.movieList[i].imageLink) {
+                                loadImage(data.movieList[i].imageLink, w, h, $("#Image_"+cnt), data.movieList[i].title);
+                            } else {
+                                loadImage("where.png", w, h, $("#Image_"+cnt), data.movieList[i].title);
+                            }
+                        });
+                        $("#Image_"+cnt).on('click','img',function(){
+                            window.open(data.movieList[i].imdb);
+                        });
+                    }
+                    function loadImage(path, width, height, target, title = "No title") {
+                        $("<p>        </p><br>").appendTo(target);
+                        $("<p>"+title+"</p>").appendTo(target);
+                        $('<img src="'+ path +'">').load(function() {
+                            $(this).width(width).height(height).appendTo(target);
+                        }).error(function(e) {
+                            $(this).attr("src", "where.png").width(width).height(height);
+                        });
+                    }
                 }
             }
         }).error(function (e) {
